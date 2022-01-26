8e96cf9ec1e4ed5c755129ad8f97529875e29aeb
Gregorio Moreta <gregoriomoreta4@gmail.com>
2022-01-25 20:35:39 -0500

    made a seed file with faker and uncommented rack-cors in gemfile ran bundle install

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+100.times do
+    Quote.create(
+        author: Faker::GreekPhilosophers.name,
+        quote: Faker::GreekPhilosophers.quote
+    )
+end
+
+puts "Seeded database"
